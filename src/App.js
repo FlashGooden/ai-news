bbe8f70b92f80e8ae11365699b20f14ffe6dce37
Gamerman2001 <Tro12185@gmail.com>
2020-08-05 15:03:59 -0400

    adding first command.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {useEffect} from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 
-const alanKey = process.env.KEYUP
+const alanKey = process.env.REACT_APP_KEYUP
+console.log(alanKey)
 export default function App() {
 useEffect(() => {
   alanBtn({
-    key: alanKey
+    key: alanKey,
+    onCommand: ({command})=> {
+      if(command === 'hooty') {
+        alert('you said the right command BOYYYY!!!!  ')
+      }
+    }
   })
   
 }, [])
